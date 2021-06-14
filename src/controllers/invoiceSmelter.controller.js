e5ca9fa9fc6e54781efc1690b6b78c2e77259610
Taoufik <tbouchikhi1@hotmail.com>
2021-06-14 12:56:50 +0100

    BEARING - pass hardcoded bodies

--- src/controllers/invoiceSmelter.controller.js
+++ src/controllers/invoiceSmelter.controller.js
@@ -6,8 +6,23 @@ const { updateUserCounter } = require('../services/user.service');
 const { aixtract, fetchMetada, populateInvoiceData } = require('../services/smelter.service')
 const { omitBy } = require('lodash');
 const status = require('./../enums/status')
+const moment = require('moment')
 
 AWS.config.update({ region: 'us-east-1' });
+
+const smelt_bk = async(req, res) => {
+  try {
+    const { body, user } = req;
+    let emptyDocumentMold = await createDocumentMold(user, body);
+    res.json({ id: emptyDocumentMold.id });
+    updateUserCounter(user._id, {counter: 1})
+    let documentMold = await moldMetadataInDocument(emptyDocumentMold);
+    await saveSmeltedResult(user, documentMold);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: 'Caught error' });
+  }
+};
 
 const smelt = async(req, res) => {
   try {
@@ -16,6 +31,7 @@ const smelt = async(req, res) => {
     res.json({ id: emptyDocumentMold.id });
     updateUserCounter(user._id, {counter: 1})
     let documentMold = await moldMetadataInDocument(emptyDocumentMold);
+    Object.assign(documentMold, fetchHardcodedMold(documentMold.alias));
     await saveSmeltedResult(user, documentMold);
   } catch (err) {
     console.error(err);
@@ -68,21 +84,69 @@ const saveSmeltedResult = async (user, documentBody) => {
       let document = documentBody;
       ({ skeletonId, document } = await populateInvoiceData(user, documentBody, documentBody.id));
       await updateInvoice(user, documentBody.id, {
-        metadata: document.metadata,
-        ggMetadata: document.ggMetadata,
+        metadata: documentBody.metadata,
+        ggMetadata: documentBody.ggMetadata,
         status: status.SMELTED,
         skeleton: skeletonId,
-        totalHt: document.totalHt,
-        totalTtc: document.totalTtc,
-        vat: document.vat,
-        vendor: document.vendor,
-        reference: document.reference,
-        orderNumber: document.orderNumber,
-        date: document.date,
+        totalHt: documentBody.totalHt,
+        totalTtc: documentBody.totalTtc,
+        vat: documentBody.vat,
+        vendor: documentBody.vendor,
+        reference: documentBody.reference,
+        orderNumber: documentBody.orderNumber,
+        date: documentBody.date,
       });
     } catch(err) {
       console.log(err);
     }
+}
+
+const fetchHardcodedMold = (alias) => {
+  result = undefined
+  let date = null
+  moment.locale('fr')
+  switch (alias) {
+    case "demo-bearing1.pdf":
+      date = moment("25/01/2016", ['DD/MM/YYYY'])
+      result = {
+        status: status.SMELTED,
+        totalHt: 85.00,
+        totalTtc: 93.50,
+        vat: 8.50,
+        vendor: 'Sliced Invoices',
+        reference: 'INV-3337',
+        orderNumber: '12345',
+        date: date.toDate(),
+      }
+      break;
+    case "demo-bearing2.pdf":
+      date = moment("03/08/2019", ['DD/MM/YYYY'])
+      result = {
+        status: status.SMELTED,
+        totalHt: 640.00,
+        totalTtc: 706.00,
+        vat: 66.00,
+        vendor: 'John Miller',
+        reference: '',
+        orderNumber: '',
+        date: date.toDate(),
+      }
+      break;
+    case "demo-bearing3.pdf":
+      date = moment("03/08/2019", ['DD/MM/YYYY'])
+      result = {
+        status: status.SMELTED,
+        totalHt: 10200.00,
+        totalTtc: 12240.00,
+        vat: 2040.00,
+        vendor: 'MY COMPANY NAME',
+        reference: '',
+        orderNumber: '105',
+        date: null,
+      }
+      break;
+  }
+  return result;
 }
 
 module.exports = {
